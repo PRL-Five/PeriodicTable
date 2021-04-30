1cb1e2165645cad5d3b68d62216ab5ee2fa6b485
PRL-Five <70338293+PRL-Five@users.noreply.github.com>
2021-04-30 22:26:32 +0530

    Update tpt2.js

--- tpt2.js
+++ tpt2.js
@@ -1,11 +1,12 @@
+document.getElementById("intro").style.display = 'none';
+document.getElementById("choice").style.display = 'none';
 function start() {
-    document.getElementById("reset").innerHTML=`<h1 class="thic">The Periodic Table</h1>
-    <p id="js1">The periodic table, also known as the periodic table of elements, is a tabular display of
-    the chemical elements, which are arranged by atomic number, electron configuration, and recurring chemical 
-    properties. The structure of the table shows periodic trends. The seven rows of the table, called periods,
-    generally have metals on the left and nonmetals on the right. The columns, called groups, contain elements
-    with similar chemical behaviours. Six groups have accepted names as well as assigned numbers: for example,
-    group 17 elements are the halogens; and group 18 are the noble gases. Also displayed are four simple rectangular
-    areas or blocks associated with the filling of different atomic orbitals.</p>
-    <img src="https://www.sciencenewsforstudents.org/wp-content/uploads/2019/11/1080_SS_periodic_table_0-1028x579.png" class="img1">`
+    document.getElementById("reset").style.display = 'none';
+    document.getElementById("intro").style.display = 'block';
+   let x = document.getElementById("inp").value;
+   document.getElementById("Next1").innerHTML = `Ready for more, ${x}?`
+}
+function start2() {
+    document.getElementById("choice").style.display = 'block';
+    document.getElementById("intro").style.display = 'none';
 }
